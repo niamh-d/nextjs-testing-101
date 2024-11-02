493becbb7c2ede3d3f0b6edd61c6f1084245f12f
Niamh <niamh.linguist@gmail.com>
2024-11-02 14:50:08 +0200

    getbyrole test passing

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,8 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import Home from "@/app/page";
 
-it("should have Docs text", () => {
-  render(<Home />);
-  const docsText = screen.getByText(/Docs/i);
-  expect(docsText).toBeInTheDocument();
+describe("Home", () => {
+  it("should have Docs text", () => {
+    render(<Home />);
+    const docsText = screen.getByText(/Docs/i);
+    expect(docsText).toBeInTheDocument();
+  });
+
+  it('should contain the text "information"', () => {
+    render(<Home />);
+    const examplesText = screen.getByText(/examples/i);
+    expect(examplesText).toBeInTheDocument();
+  });
+
+  it('should have a link called "learn"', () => {
+    render(<Home />);
+    const learnLink = screen.getByRole("link", {
+      name: /learn/i,
+    });
+    expect(learnLink).toBeInTheDocument();
+  });
 });
